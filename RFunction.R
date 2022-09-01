958d221e05515ab3f9d9125e5075c85d28fb168e
andreakoelzsch <akoelzsch@ab.mpg.de>
2022-09-01 11:30:38 +0200

    update data.df for map

--- RFunction.R
+++ RFunction.R
@@ -394,8 +394,11 @@ rFunction <- function(data, window="all", upX=0, downX=0, speedvar="speed", maxs
         
         map <- get_map(bbox(extent(data)),source="osm",force=TRUE)
         
+        data.df <- data.frame(coordinates(data),as.data.frame(moveStack(data)))
+        names(data.df)[1:2] <- c("location.long","location.lat")
+        
         out <- ggmap(map) +
-          geom_path(data=as.data.frame(data),aes(x=location.long,y=location.lat,group=individual.local.identifier),colour="blue") +
+          geom_path(data=data.df,aes(x=location.long,y=location.lat,group=trackId),colour="blue") +
           geom_point(data=prop.rest.df,aes(x=rest.mean.long,y=rest.mean.lat),colour="red",size=3)
         ggsave(out, file = paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"rest_sites_onTracks.pdf"))
         
